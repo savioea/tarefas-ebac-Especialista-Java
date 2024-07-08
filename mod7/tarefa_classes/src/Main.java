4fad66e7371934bff70220bfb8190661332f378e
Sávio Emerick <savio.ea@hotmail.com>
2024-07-08 13:37:10 -0300

    Mainjava atualizado: testando getters e setters

--- mod7/tarefa_classes/src/Main.java
+++ mod7/tarefa_classes/src/Main.java
@@ -10,6 +10,11 @@ public class Main {
         caminhao1.setCor("Cinza");
         caminhao1.setNumeroDeEixos(3);
 
+        // Testando se os setters e getters funcionaram
+        System.out.println("Modelo do caminhão: " + caminhao1.getModelo());
+        System.out.println("Marca do caminhão: " + caminhao1.getMarca());
+        System.out.println("Cor do caminhão: " + caminhao1.getCor());
+        System.out.println("Número de Eixos do caminhão: " + caminhao1.getNumeroDeEixos());
 
         // Usa o método mais atual
         caminhao1.imprimeCargaMaxima(caminhao1.getNumeroDeEixos());
