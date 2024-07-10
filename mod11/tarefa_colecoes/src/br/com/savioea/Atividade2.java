1db2aa550db906e3e5f3fc5e614bfdf1ee0f09ae
Sávio Emerick <savio.ea@hotmail.com>
2024-07-10 11:02:51 -0300

    Pequena correção no Atividade2.java - Formato de nove/sexo e erros de portugês nos comentários

--- mod11/tarefa_colecoes/src/br/com/savioea/Atividade2.java
+++ mod11/tarefa_colecoes/src/br/com/savioea/Atividade2.java
@@ -9,32 +9,29 @@ public class Atividade2 {
         // Definição do Scanner que lerá os valores do console
         Scanner leitorDoConsole = new Scanner(System.in);
 
-        // Definição da String que sofrerá o .split() e será então processada
+        // Definição da String 'listaDeNomesString' que sofrerá o .split() e será então processada
         System.out.println("Digite uma lista de nomes no modelo:");
-        System.out.println("[nome 1] [SEXO 1], [nome 2] [SEXO 2], [nome 3] [SEXO 3], ... [nome n] [SEXO n]");
+        System.out.println("[nome 1] - [SEXO 1], [nome 2] - [SEXO 2], [nome 3] - [SEXO 3], ... [nome n] - [SEXO n]");
         String listaDeNomesString = leitorDoConsole.nextLine();
 
-        // Definição da Map (HashMap) que receberá os valores dos nomes e sexo
+        // Definição do Map (HashMap) que receberá os valores de sexo (como chaves) e uma lista de nomes
         Map<String, List<String>> pessoaMap = new HashMap<String, List<String>>();
 
         /*
          * Para cada iteração a variavel pessoa recebe o valor de um item do array
-         * resultante do .split()
+         * resultante do .split(",")
          */
         for (String pessoa : listaDeNomesString.split(","))
         {
 
             /*
-             * Como cada item do array está no formato '[nome x] [SEXO x]' o que separa os
-             * nomes e sexos são um epaço (" "). O array resultante do .split(" ") vai ter na posição
+             * Como cada item do array está no formato '[nome x] - [SEXO x]' o que separa os
+             * nomes e sexos é um traço ("-"). O array resultante do .split("-") vai ter na posição
              * [0] o nome e na posição [1] o sexo correspondente. Esses valores são salvos nas
              * variáveis nome e sexo respectivamente.
-             *
-             * OBS: Foi necessário o uso do método .trim(), para retirar os espaços no início e final,
-             * pois os mesmos estava fazendo com que o array tivesse itens vazios.
              */
-            String nome = pessoa.trim().split(" ")[0];
-            String sexo = pessoa.trim().split(" ")[1];
+            String nome = pessoa.split("-")[0].trim();
+            String sexo = pessoa.split("-")[1].trim();
 
             /*
              * Se a chave com o sexo desta iteração já existir, puxamos a lista correspondente
