8b9d063ce48e2bd3298fed7d2691b2c0253e6bf2
Sávio Emerick <savio.ea@hotmail.com>
2024-09-10 15:53:48 -0300

    Adicionados comentarios para melhor entendimento dos testes

--- mod23/testes_parte1/src/br/com/savioea/AtividadeStreamsTest.java
+++ mod23/testes_parte1/src/br/com/savioea/AtividadeStreamsTest.java
@@ -7,6 +7,7 @@ import java.util.List;
 
 public class AtividadeStreamsTest {
 
+    // Teste que verifica se a lista de nomes está sendo criada corretamente
     @Test
     public void testPopulaListaDeNomes(){
         AtividadeStreams atividadeStreams = new AtividadeStreams();
@@ -16,12 +17,19 @@ public class AtividadeStreamsTest {
         Assertions.assertNotEquals(0, listaDeNomes.size());
     }
 
+    // Teste que verifica se após o filtro existem apenas mulheres na lista
     @Test
     public void testFiltraListaMulheres(){
+        // Cria o objeto atividadeStreams
         AtividadeStreams atividadeStreams = new AtividadeStreams();
+
+        // Adiciona nomes à lista de nomes
         atividadeStreams.populaListaDeNomes("Sávio - M, Letícia - F, Sara - F, Cynthia - F, Arthur - M, Mateus - M, Paula - F, Fabrício - M");
+
+        // Faz o filtro da lista anterior para apenas haverem mulheres
         List<String> listaDeMulheres = atividadeStreams.filtraListaMulheres(atividadeStreams.getListaDeNomes());
 
+        // Testa cada nome da lista retornada, para verificar se todos possuem " - F"
         listaDeMulheres.forEach(nome -> Assertions.assertTrue(nome.contains(" - F")));
     }
 }
